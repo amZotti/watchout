e5fe62e5a99cdb8caaac9d65c30b1ac0f406a1cc
amZotti <anthonymzotti@gmail.com>
2015-02-13 15:44:34 -0800

    Style document using D3

---
+++ initD3.js
@@ -0,0 +1,61 @@
+
+function scoreBoardSetup() {
+  var sexyBlue = '#6ABEDB';
+  var color = '#F0F0F0';
+  d3.select('.scoreboard')
+    .selectAll('div')
+    .style('background-color', function() {
+      return sexyBlue;
+    })
+    .style('color', function() {
+      return color;
+    })
+    .style('font-family', function() {
+      return 'sans-serif';
+    })
+    .style('font-weight', function() {
+      return 'bold';
+    });
+}
+
+function boardSetup() {
+  var height = "calc(" + window.innerHeight + 'px - 5em)';
+  var width = '100%';
+  var BGcolor = 'black';
+  d3.select('.gameboard')
+    .style('width', function() {
+      return width;
+    })
+    .style('height', function() {
+      return height;
+    })
+    .style('background', function() {
+      return BGcolor;
+    });
+}
+
+function documentSetup() {
+  var BGcolor = '#757571';
+  d3.select('body').style('margin', function() {
+    return '0px';
+  })
+  .style('padding', function() {
+    return '0px';
+  });
+
+  d3.select('main').style('background-color', function() {
+    return BGcolor;
+  })
+  .style('padding', function() {
+    return '1em';
+  });
+}
+
+function init() {
+  documentSetup();
+  boardSetup();
+  scoreBoardSetup();
+
+}
+
+init();
