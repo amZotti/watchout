728e7ff6fbe6adb6ae51835fdcbe1e599a410faf
amZotti <anthonymzotti@gmail.com>
2015-02-13 16:48:38 -0800

    Draw the enemies in an svg element.

--- watchout.js
+++ watchout.js
@@ -5,11 +5,16 @@ function generateSVG(num) {
   array.length = num;
   var string = 'test';
 
-  var asteroid = {
-    width: 100,
-    height: 100,
-    radius: 40,
-    color: 'brown'
+  function asteroid(prop){
+    var randomFactor = Math.random() * 40 + 1;
+    var asteroid = {
+      width: 100,
+      height: 100,
+      radius: randomFactor,
+      color: 'brown'
+    }
+
+    return asteroid[prop];
   }
 
   d3.select('.gameboard')
@@ -20,16 +25,16 @@ function generateSVG(num) {
     .append('svg')
       .append('circle')
         .attr('cx', function() {
-          return asteroid.width / 2;
+          return asteroid("width") / 2;
         })
         .attr('cy', function() {
-          return asteroid.height / 2;
+          return asteroid("height") / 2;
         })
         .attr('fill', function() {
-          return asteroid.color;
+          return asteroid("color");
         })
         .attr('r', function() {
-          return asteroid.radius;
+          return asteroid("radius");
         });
 
 }
