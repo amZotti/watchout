7a9adebf5afdcb66e8feb75313a0cddc3546cd91
amZotti <anthonymzotti@gmail.com>
2015-02-13 16:38:37 -0800

    Implement SVG generating using d3

--- watchout.js
+++ watchout.js
@@ -1,19 +1,36 @@
 // start slingin' some d3 here.
 
-function generateElement(num) {
+function generateSVG(num) {
   var array = [];
   array.length = num;
   var string = 'test';
+
+  var asteroid = {
+    width: 100,
+    height: 100,
+    radius: 40,
+    color: 'brown'
+  }
+
   d3.select('.gameboard')
-    .selectAll('p')
+    .selectAll('svg')
     .data(array)
-    .text(function(d){
-      return string + d;
-    })
+    //new
     .enter()
-    .append('p')
-    .text(function(d){
-      return string + d;
-    });
+    .append('svg')
+      .append('circle')
+        .attr('cx', function() {
+          return asteroid.width / 2;
+        })
+        .attr('cy', function() {
+          return asteroid.height / 2;
+        })
+        .attr('fill', function() {
+          return asteroid.color;
+        })
+        .attr('r', function() {
+          return asteroid.radius;
+        });
+
 }
 generateSVG(10);
